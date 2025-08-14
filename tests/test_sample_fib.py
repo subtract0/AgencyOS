621e35a28435edff69e530e9a33c2205894372a2
Arsenii Shatokhin <arseny9795@gmail.com>
2025-08-14 14:21:55 +0400

    Added planner agent and complete todo tool

---
+++ tests/test_sample_fib.py
@@ -0,0 +1,16 @@
+import pytest
+from fib import fib_iter, fib_sequence
+
+
+def test_fib_iter_small():
+    assert fib_iter(0) == 0
+    assert fib_iter(1) == 1
+    assert fib_iter(2) == 1
+    assert fib_iter(10) == 55
+
+
+def test_fib_sequence():
+    assert fib_sequence(0) == [0]
+    assert fib_sequence(1) == [0, 1]
+    assert fib_sequence(5) == [0, 1, 1, 2, 3, 5]
+
